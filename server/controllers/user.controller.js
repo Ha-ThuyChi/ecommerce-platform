2556c0c686e72bfbcb84660f684ed13faa43b00e
THuy Chi <chi.hathuy15@gmail.com>
2024-06-15 20:25:48 +0700

    Add CRUD user (server)

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,6 +2,7 @@ const db = require("../models");
 const User = db.user;
 const Role = db.role;
 const User_role = db.user_role;
+const Cart = db.cart;
 const { hashPassword } = require("./hashPassword");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
@@ -18,29 +19,35 @@ exports.createUser = async (req, res) => {
             res.status(409).send({success: false, message: "email is existed."})
             return;
         };
-        // Email is valid to create new user and buyer account
+        // Email is valid to create new user account, buyer account and cart
         const password = req.body.password;
         const hashedPassword = await hashPassword(password);
+        // User account
         const newUser = await User.create({
             name: req.body.name,
             email: req.body.email,
             dob: req.body.dob,
             password: hashedPassword,
+            address: req.body.address,
+            phone: req.body.phone
         });
+        // Buyer account
         const role = await Role.findOne({where: {name: "Buyer"}});  // Find buyer role
-        try {
-            const newUserRole = User_role.create({
-                userId: newUser.id,
-                roleId: role.id,
-            })
-            if (newUserRole) {
-                res.status(200).send({success: true, message: "User account is created."})
-            } 
-        } catch (error) {
-            res.status(500).send({success: false, message: error.message})
-        };
+        const newUserRole = await User_role.create({
+            userId: newUser.id,
+            roleId: role.id,
+        })
+        // Cart
+        const newCart = await Cart.create({
+            userId: newUser.id,
+        });
+        if (newUser && newUserRole && newCart) {
+            res.status(200).send({success: true, message: "User account is created."});
+        } else {
+            res.status(400).send({success: true, message: "Fail to create a new account."});
+        }
     } catch (error) {
-        res.status(500).send({success: false, message: error.message})
+        res.status(500).send({success: false, message: error.message});
     };
 };
 
@@ -100,3 +107,70 @@ exports.createSellerAccount = async (req, res) => {
     };
 };
 
+exports.viewUser = async (req, res) => {
+    try {
+        const user = await User.findOne({
+            id: req.params.userId,
+        });
+        if (user == null) {
+            res.status(404).send({status: false, message: "User is not found."});
+            return;
+        }
+        res.status(200).send({status: true, message: user});
+    } catch (error) {
+        res.status(500).send({success: false, message: error.message});
+    };
+};
+
+exports.editUser = async (req, res) => { 
+    try {
+        // Check if input email is existed
+        const email = await  User.findOne({
+            where: {
+                email: req.body.email,
+            }
+        });
+        if (email) {
+            res.status(409).send({success: false, message: "email is existed."})
+            return;
+        };
+        const isEdit = await User.update(
+            {
+                name: req.body.name,
+                email: req.body.email,
+                dob: req.body.dob,
+                address: req.body.address,
+                phone: req.body.phone
+            },{
+                where: {
+                    id: req.body.userId
+            }}
+        );
+        if (isEdit != 1) {
+            res.status(404).send({status: false, message: "User is not found."});
+            return;
+        }
+        res.status(200).send({status: true, message: "User is updated."});
+    } catch (error) {
+        res.status(500).send({success: false, message: error.message});
+    };
+};
+
+exports.deleteUser = async (req, res) => {
+    try {
+        const isDelete = await User.update({
+            activeStatus: "Closed",
+        }, {
+            where: {
+                id: req.params.userId,
+            }
+        });
+        if (isDelete != 1) {
+            res.status(404).send({success: false, message: "No user is delete."});
+            return;
+        }
+        res.status(200).send({status: true, message: "User is deleted."});
+    } catch (error) {
+        res.status(500).send({success: false, message: error.message});
+    };
+};
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -6,6 +6,11 @@ const authenticateToken = require("./authenticate.route");
 // router.use(authenticateToken);
 
 router.get("/sign-in", controller.signIn);
+router.post("/create-user", controller.createUser);
 router.post("/create-seller-account",authenticateToken, controller.createSellerAccount);
+router.get("/view-user/:userId", controller.viewUser);
+router.put("/edit-user", controller.editUser);
+router.put("/delete-user/:userId", controller.deleteUser);
+
 
 module.exports = router;
